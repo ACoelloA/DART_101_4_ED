e13f52f54fa1c4a4bc8183886995e0db4d95643e
ACoelloA <acoello@unidas.com.ec>
2024-11-06 19:18:11 -0500

    add: archivo comentarios

---
+++ 01_HOLA_MUNDO/ABRAHAM_COELLO/comentarios.dart
@@ -0,0 +1,22 @@
+void main(){
+//comentario
+
+print("hola mundo");
+print("hola mundo");
+print("hola mundo");
+
+/*
+print("hola mundo");
+print("hola mundo");
+print("hola mundo");
+*/
+
+/// Comentarios de documentacion
+/// lleva un / adicional
+/// 
+/// 
+/// 
+
+
+
+}
---
+++ 02_VARIABLES/ABRAHAM_COELLO/CLASE/variables.dart
@@ -0,0 +1,16 @@
+void main() {
+  //var
+  var nombre = "Abraham";
+  print(nombre);
+
+  //late
+  late String edad;
+  edad = "12";
+
+  print(edad);
+
+  String apellido = "Coello";
+  print(apellido);
+  
+
+}
